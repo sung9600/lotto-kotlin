2c6e78cab003083e0fe0cc2aa59a3276f27b3f36
sung9600 <sung96002@naver.com>
2020-06-28 21:49:40 +0900

    lotto-kotlin

--- app/src/main/java/edu/skku2/map/lotto_kotlin/MainActivity.kt
+++ app/src/main/java/edu/skku2/map/lotto_kotlin/MainActivity.kt
@@ -2,11 +2,98 @@ package edu.skku2.map.lotto_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.util.Log
+import android.view.Surface
+import android.view.TextureView
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
 
+private const val REQUEST_CODE_PERMISSIONS =10
+private val REQUIRED_PERMISSIONS= arrayOf(android.Manifest.permission.CAMERA)
 class MainActivity : AppCompatActivity() {
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var outputDirectory:File
+    private lateinit var cameraExecutor:ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(allPermissionsGranted()){
+            startCamera()
+        }
+        else{
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
     }
+
+    private fun startCamera(){
+
+        val cameraProviderFuture=ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider:ProcessCameraProvider=cameraProviderFuture.get()
+            preview=Preview.Builder().build()
+            val cameraSelector=CameraSelector
+                .Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            try{
+                cameraProvider.unbindAll()
+                camera=cameraProvider
+                    .bindToLifecycle(this,cameraSelector,preview)
+                preview?.setSurfaceProvider(viewFinder.createSurfaceProvider())
+            }catch (exc:Exception){
+                Log.e("Fail", exc.toString())
+            }
+        },ContextCompat.getMainExecutor(this))
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode== REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera()
+            }
+            else{
+                Toast.makeText(this,"notgranted",Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
 }
